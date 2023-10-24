3e24eada395c4b083d02e41ed61f80ada86b654b
Artur Zagirov <zagart47@gmail.com>
2023-10-24 22:52:14 +0500

    fix: migrations

--- person/migrations/0001_add_tables.sql
+++ person/migrations/0001_add_tables.sql
@@ -1,11 +1,19 @@
 SELECT 'up SQL query';
-CREATE TABLE persons
+CREATE TABLE IF NOT EXISTS persons
 (
-    id         BIGSERIAL PRIMARY KEY,
+    id         SERIAL PRIMARY KEY ,
     name       VARCHAR(255) NOT NULL,
     surname    VARCHAR(255),
     patronymic VARCHAR(255),
-    age        INT,
+    age        INTEGER,
     gender     VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS countries
+(
+    person_id   BIGINT REFERENCES persons (id) ON DELETE CASCADE,
+    country_id  VARCHAR(255) NOT NULL,
+    probability FLOAT,
+    PRIMARY KEY (person_id, country_id)
 );
 SELECT 'down SQL query';
