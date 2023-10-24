6e4c92eb1af876520d55274eb984fcb4a9aded3c
Artur Zagirov <zagart47@gmail.com>
2023-10-24 22:56:49 +0500

    feat: add Country struct, add Country struct to Create method

--- person/model.go
+++ person/model.go
@@ -1,10 +1,17 @@
 package person
 
 type Person struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	Surname    string `json:"surname"`
-	Patronymic string `json:"patronymic"`
-	Age        int    `json:"age"`
-	Gender     string `json:"gender"`
+	ID         string    `json:"id"`
+	Name       string    `json:"name"`
+	Surname    string    `json:"surname"`
+	Patronymic string    `json:"patronymic"`
+	Age        int       `json:"age"`
+	Gender     string    `json:"gender"`
+	Country    []Country `json:"country"`
+}
+
+type Country struct {
+	PersonId    int     `json:"person_id"`
+	CountryId   string  `json:"country_id"`
+	Probability float32 `json:"probability"`
 }
--- person/postgresql.go
+++ person/postgresql.go
@@ -3,6 +3,7 @@ package person
 import (
 	"context"
 	"dego/pkg/client/postgresql"
+	"errors"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgconn"
 )
@@ -12,40 +13,68 @@ type repository struct {
 }
 
 func (r *repository) Create(ctx context.Context, person Person) (string, error) {
-	q := `
+	personQuery := `
 		INSERT INTO public.persons (name, surname, patronymic, age, gender)
 		VALUES ($1, $2, $3, $4, $5)
 		RETURNING (id)`
-	if err := r.client.QueryRow(ctx, q, person.Name, person.Surname, person.Patronymic, person.Age, person.Gender).Scan(&person.ID); err != nil {
-		if pgErr, ok := err.(*pgconn.PgError); ok {
+	if err := r.client.QueryRow(ctx, personQuery, person.Name, person.Surname, person.Patronymic, person.Age, person.Gender).Scan(&person.ID); err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
 			fmt.Println(fmt.Sprintf("SQL Error: %s, Detail: %s, Where: %s", pgErr.Message, pgErr.Detail, pgErr.Where))
 		}
-		return "", err
 	}
+	for _, country := range person.Country {
+		countryQuery := `
+			INSERT INTO countries (person_id, country_id, probability)
+			VALUES ($1, $2, $3)`
+		if err := r.client.QueryRow(ctx, countryQuery, person.ID, country.CountryId, country.Probability).Scan(&person.ID); err != nil {
+			var pgErr *pgconn.PgError
+			if errors.As(err, &pgErr) {
+				fmt.Println(fmt.Sprintf("SQL Error: %s, Detail: %s, Where: %s", pgErr.Message, pgErr.Detail, pgErr.Where))
+			}
+		}
+	}
+
 	return person.ID, nil
 }
 
 func (r *repository) FindAll(ctx context.Context) (p []Person, err error) {
-	q := `
+	personQuery := `
 		SELECT id, name, surname, patronymic, age, gender FROM public.persons
 		`
-	rows, err := r.client.Query(ctx, q)
+	rows, err := r.client.Query(ctx, personQuery)
 	if err != nil {
 		return nil, err
 	}
 	persons := make([]Person, 0)
+	countryQuery := `
+		SELECT person_id, country_id, probability FROM public.countries WHERE person_id = $1
+		`
 	for rows.Next() {
 		var p Person
 		err = rows.Scan(&p.ID, &p.Name, &p.Surname, &p.Patronymic, &p.Age, &p.Gender)
 		if err != nil {
 			return nil, err
 		}
-		persons = append(persons, p)
-
 		if err = rows.Err(); err != nil {
 			return nil, err
 		}
+		countryRows, err := r.client.Query(ctx, countryQuery, p.ID)
+		if err != nil {
+			return nil, err
+		}
+		for countryRows.Next() {
+			var c Country
+			err = countryRows.Scan(&c.PersonId, &c.CountryId, &c.Probability)
+			if err != nil {
+				return nil, err
+			}
+			p.Country = append(p.Country, c)
+		}
+
+		persons = append(persons, p)
 	}
+
 	return persons, nil
 }
 
